44629344345f16439de088445b4222e6716b0e01
DragonWarrior9842 <aasblko@gmail.com>
2026-07-11 10:04:33 +0530

    Update app.py

--- Project_06_Logistic_Regression_Model_of_Insurance/app.py
+++ Project_06_Logistic_Regression_Model_of_Insurance/app.py
@@ -59,7 +59,7 @@ sl.markdown(
 
 col_logo, col_title = sl.columns([1, 5])
 with col_logo:
-    sl.image("insurance.jpg", width=400)
+    sl.image(os.path.join(BASE_DIR, "insurance.jpg"), width=400)
 with col_title:
     sl.title("Insurance Prediction")
     sl.caption("A simple ML demo that predicts insurance need based on age.")
